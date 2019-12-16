fe229e663fbdf3011230818dfb588edf51339569
achilles schmelzer <achillesdev@fastmail.com>
2019-12-16 15:21:00 -0800

    Remove src/index.html

--- ops/build.js
+++ ops/build.js
@@ -12,24 +12,47 @@ const util = require("util");
 const readdir = util.promisify(fs.readdir);
 const readfile = util.promisify(fs.readFile);
 const writefile = util.promisify(fs.writeFile);
+const exec = util.promisify(require('child_process').exec);
 
 const INDEX_TEMPLATE = "templates/index.html";
-const INDEX = "src/index.html";
-const ARTICLES = "articles";
+const INDEX_HTML = "src/index.html";
+const MANIFEST = require(".././manifest.json");
+const TEMPLATE = "templates/article.html";
+
+const getContent = async (inFile, type) => {
+  const { stdout, stderr } =  await exec(
+    `pandoc -f markdown -t html ${inFile}`
+  );
+  return stdout;
+}
+
+const getCompiledIncludes = async () => {
+  const includes = {};
+  for (let i = 0; i < MANIFEST.length; i++) {
+    const { params, content_path, template_path, include_path } = MANIFEST[i];
+    const { title, type, post_date, display_date } = params;
+    let template = String(await readfile(template_path));
+    Object.entries(params).forEach(([key, value]) => {
+      const search = new RegExp(`{{${key}}}`, "g");
+      template = template.replace(search, value);
+    });
+    const content = await getContent(content_path, type);
+    template = template.replace(/{{content}}/g, content);
+    includes[include_path] = template;
+  }
+  return includes;
+}
 
 const main = async () => {
-  const articleNames = await readdir(ARTICLES)
-  const articles = await Promise.all(articleNames.map(name => {
-    return readfile(`${ARTICLES}/${name}`).then(article => [name, article]);
-  }));
+  const includes = await getCompiledIncludes();
 
   let template = String(await readfile(INDEX_TEMPLATE));
-  articles.forEach(([name, article]) => {
-    const includeDirective = `{{include:articles/${name}}}`;
-    template = template.replace(includeDirective, article);
+
+  Object.entries(includes).forEach(([includePath, include]) => {
+    template = template.replace(`{{include:${includePath}}}`, include);
   });
 
-  writefile(INDEX, template);
+  writefile(INDEX_HTML, template);
 };
 
 main();
